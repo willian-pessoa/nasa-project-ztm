4996cc1544223f25d58cccb275fb918f4358ceb1
willian-pessoa <willianpessoadeabreu@gmail.com>
2022-09-13 21:45:42 -0300

    add connection to atlas mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const http = require("http")
+const mongoose = require("mongoose")
 
 const app = require("./app")
 
@@ -6,11 +10,22 @@ const { loadPlanetsData } = require("./models/planets.model")
 
 const PORT = process.env.PORT || 8000
 
+const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGODB_PASSWORD}@nasacluster.6hbbqnp.mongodb.net/?retryWrites=true&w=majority`
+
 const server = http.createServer(app)
 
-async function startServer(){
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!!")
+})
+
+mongoose.connection.on("error", (err) => {
+  console.error(err)
+})
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL)
   await loadPlanetsData();
-  
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
   })
